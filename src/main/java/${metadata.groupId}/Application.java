7a93f8080d8538e4d88ebf0822e17d02058c272a
zhengzheming <7upcat@gmail.com>
2018-07-11 10:37:18 +0800

    优化了模板，适配 reap-cli 0.0.3 版本自动生成交易的功能

--- src/main/java/${metadata.groupId}/Application.java
+++ src/main/java/${metadata.groupId}/Application.java
@@ -27,7 +27,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication(scanBasePackages = {"org.reap", "<%=metadata.groupId%>"})
+<% if( metadata.groupId=== 'org.reap') {%>
+@SpringBootApplication(scanBasePackages = "org.reap")<% } else {%>
+@SpringBootApplication(scanBasePackages = { "org.reap", "<%=metadata.groupId%>" })<% }%>
 @EnableDiscoveryClient
 public class Application {
 
--- src/web/generation/readme.md
+++ src/web/generation/readme.md
@@ -9,5 +9,17 @@
     { "key": "age", "name": "年龄", "type": "Date", "editable": true, "searchable": true, "required": true }
   ]
 }
-
 ```
+
+对于每个数据域 （`field`） 的定义属性包含以下
+  - `key`  数据域 id ，命名小写开头驼峰式，必输
+  - `name` 数据域中文名称，前端的表单、表格使用，必输
+  - `type` 数据类型，支持以下 String、Integer、Long、Date、Timestamp、BigDecimal（区分大小写），默认为 String
+  - `primaryKey` 主键标识，如果未指定自动设置为 id，类型支持 Long、String
+  - `required` 是否必输，默认 false
+  - `editable` 是否可修改，默认 flase
+  - `searchable` 是否做为查询条件 ，默认 false 
+
+
+
+
