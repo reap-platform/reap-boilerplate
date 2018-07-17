77010cce20f04b116905df6f8374ebc48ff04733
zhengzheming <7upcat@gmail.com>
2018-07-17 08:25:59 +0800

    模板同 reap-cli 同步

--- src/web/mock/devConfig.js
+++ src/web/mock/devConfig.js
@@ -4,7 +4,7 @@ export default {
   // 本地开发阶段的通讯配置
   devComm: {
     // proxy 为 true 时会使用配置的 urlPrefix 而非向本地 mock server 发送请求，用于本地开发的页面同后台联调
-    proxy: false,
+    proxy: true,
     urlPrefix: 'http://localhost:8080',
   },
   devSession: {
